62c8b58b74a36f2110f162f315dd7cccb6435deb
Sara Crowbridge <sara.crowbridge@hotmail.com>
2021-03-09 15:30:47 -0600

    displaying posts

--- packages/books/src/components/index.js
+++ packages/books/src/components/index.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { connect } from "frontity"
 import Link from "@frontity/components/link"
+import Switch from "@frontity/components/switch"
+import List from "./list"
+import Post from "./post"
+import Page from "./page"
 
 const Root = ({ state }) => {
+    const data = state.source.get(state.router.link)
     return (
         <>
             <h1>Hello Frontity</h1>
@@ -16,6 +21,14 @@ const Root = ({ state }) => {
                 <br />
                 <Link link="/contact">Contact Us</Link>
             </nav>
+            <hr />
+            <main>
+                <Switch>
+                    <List when={data.isArchive} />
+                    <Post when={data.isPost} />
+                    <Page when={data.isPage} />
+                </Switch>
+            </main>
         </>
     )
 }
