06f683d3c311b4bae33387e24399fdbe5f2830cd
Sara Crowbridge <sara.crowbridge@hotmail.com>
2021-03-09 14:21:39 -0600

    Added a menu

--- node_modules/books/src/components/index.js
+++ node_modules/books/src/components/index.js
@@ -1,13 +1,23 @@
 import React from "react"
 import { connect } from "frontity"
+import Link from "@frontity/components/link"
 
-const Root = ( {state }) => {
+const Root = ({ state }) => {
     return (
         <>
             <h1>Hello Frontity</h1>
             <p>Current URL: {state.router.link}</p>
+            <nav>
+                <Link link="/">Home</Link>
+                <br />
+                <Link link="/author-bio">About</Link>
+                <br />
+                <Link link="/book">Books</Link>
+                <br />
+                <Link link="/contact">Contact Us</Link>
+            </nav>
         </>
     )
 }
 
-export default connect(Root);
+export default connect(Root)
